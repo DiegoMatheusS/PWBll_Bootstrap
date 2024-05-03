c132f61f87239eee862cff1e55593dafccdb4135
dms.diego.2019@gmail.com <you@example.com>
2024-05-02 22:33:03 -0300

    feat: adicionado cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,10 @@ import { HomeComponent } from './components/home/home.component';
 import { LOCALE_ID } from '@angular/core';
 import localePt from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
+import { CadastrarProdutoComponent } from './components/produtos/cadastrar-produto/cadastrar-produto.component';
 import { ListarProdutosComponent } from './components/produtos/listar-produtos/listar-produtos.component';
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
 
 
 
@@ -23,12 +25,14 @@ registerLocaleData(localePt)
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    ListarProdutosComponent
+    ListarProdutosComponent,
+    CadastrarProdutoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    SharedModule
   ],
 
   providers: [
